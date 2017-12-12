d35d2ebfcf4b4ffca7fd40d3c326a1b1e6976c9f
adel <navracsics.adel@gmail.com>
2017-12-12 13:34:24 +0100

    finished exercise1

---
+++ week-04/day-2/Green Fox organization/src/PallidaClass.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class PallidaClass {
+  private String className;
+  private ArrayList<Student> students;
+  private ArrayList<Mentor> mentors;
+
+  public PallidaClass(String className) {
+    this.className = className;
+    students = new ArrayList<>();
+    mentors = new ArrayList<>();
+  }
+
+  public void PallidaClass(String className){
+
+  }
+
+  public void addStudent(Student student){
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println(className + " class has " + students.size() + " students and " + mentors.size() + " mentors.");
+  }
+}
