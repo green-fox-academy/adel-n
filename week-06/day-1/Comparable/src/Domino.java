bc25d55b4f660d426e4b1e4108ed9ecf34a812dc
adel <navracsics.adel@gmail.com>
2018-01-02 13:17:19 +0100

    add exercise2

---
+++ week-06/day-1/Comparable/src/Domino.java
@@ -0,0 +1,27 @@
+public class Domino implements Comparable<Domino> {
+  int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[] {valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public int compareTo(Domino compareDominoes) {
+    int compare0 = compareDominoes.getValues()[0];
+    int compare1 = compareDominoes.getValues()[1];
+
+    if (this.getValues()[0] != compare0) {
+      return this.getValues()[0] - compare0;
+    } else {
+      return this.getValues()[1] - compare1;
+    }
+  }
+}
