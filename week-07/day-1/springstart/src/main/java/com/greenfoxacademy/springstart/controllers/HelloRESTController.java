ec48bf7da2b905e63d5b204a5687309d1fb3b716
adel <navracsics.adel@gmail.com>
2018-01-08 14:17:14 +0100

    first commit of exercise

---
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloRESTController {
+  AtomicLong counter = new AtomicLong(1);
+
+  @RequestMapping(value="/greeting")
+  public Greeting greeting(@RequestParam("name") String name) {
+
+    return new Greeting(counter.incrementAndGet(), name);
+  }
+}
