9f12e912d76fe1e93e0e62044a66c47e5c005687
adel <navracsics.adel@gmail.com>
2018-01-10 15:18:38 +0100

    add table

---
+++ week-07/day-3/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/ShowController.java
@@ -0,0 +1,45 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ShowController {
+
+
+  @RequestMapping(value = "/show")
+  public String firstEndPoint(Model model) {
+    BankAccount firstAccount = new BankAccount("Simba", 2000, "lion", "Zebra");
+    model.addAttribute("firstAccount", firstAccount);
+    return "show";
+  }
+
+  @RequestMapping(value = "/show/text")
+  public String secondEndPoint(Model model) {
+    String toShow = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+    model.addAttribute("toshow", toShow);
+    return "text";
+  }
+
+  @RequestMapping(value = "/show/multiple")
+  public String thirdEndPoint(Model model) {
+    List<BankAccount> accounts = new ArrayList<>();
+    BankAccount secondAccount = new BankAccount("Scar", 1000000, "lion", "Zebra");
+    BankAccount thirdAccount = new BankAccount("Rafiki", 300, "monkey", "Zebra");
+    BankAccount fourthAccount = new BankAccount("Timon", 100, "meerkat", "Bug");
+    BankAccount fifthAccount = new BankAccount("Pumba", 100, "warthog", "Bug");
+    BankAccount sixthAccount = new BankAccount("Shenzi", 0, "hyena", "Zebra");
+    accounts.add(secondAccount);
+    accounts.add(thirdAccount);
+    accounts.add(fourthAccount);
+    accounts.add(fifthAccount);
+    accounts.add(sixthAccount);
+    model.addAttribute("multiple", accounts);
+    return "multiple";
+  }
+}
