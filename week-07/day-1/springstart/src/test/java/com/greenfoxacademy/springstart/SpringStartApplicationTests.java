934eac1c0e442296e1a5204da9ce5f6aede6eff5
adel <navracsics.adel@gmail.com>
2018-01-11 09:23:52 +0100

    add small changes

--- week-07/day-1/springstart/src/test/java/com/greenfoxacademy/springstart/SpringStartApplicationTests.java
+++ week-07/day-1/springstart/src/test/java/com/greenfoxacademy/springstart/SpringStartApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringstartApplicationTests {
+public class SpringStartApplicationTests {
 
 	@Test
 	public void contextLoads() {
