eddc63a6a4e60c67ea0265c7eb07a752ce84e82d
adel <navracsics.adel@gmail.com>
2018-01-11 13:44:59 +0100

    feat(exercise 1-2) add printer and colors

---
+++ week-07/day-4/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/BlackColor.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.hellobeanworld;
+
+public class BlackColor implements MyColor {
+  @Override
+  public void printColor() {
+    System.out.println("It is black in color...");
+  }
+}
---
+++ week-07/day-4/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HelloBeanWorldApplication.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.hellobeanworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBeanWorldApplication implements CommandLineRunner{
+	@Autowired
+	Printer printer;
+
+	@Autowired
+  RedColor redColor;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBeanWorldApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		printer.log("hei!");
+		redColor.printColor();
+	}
+}
